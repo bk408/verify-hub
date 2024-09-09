219fe8be9e6f33b2a3f062475cf550cf4de4df98
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-09-10 01:35:47 +0530

    login functionality created for server

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,30 +1,68 @@
-
-
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const UserModal = require("../models/user");
+
+const jwt = require("jsonwebtoken");
 
 // signup functionality
 const signup = async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
-      const user = await UserModal.findOne({ email }); // here we check the email user enter is already exist or not if yes then user can directly login
-      // here the logic, if user exist then user can login
-      if (user) {
-        return res
-          .status(409)
-          .json({
-            message: "user is already exist, you can login",
-            success: false,
-          });
-      }
-
-        const userModal = new UserModal({ name, email, password }); // logic => if new user comes then we create a user
-        userModal.password = await bcrypt.hash(password, 10) // use bcrypt to make password in the hash form
-        await userModal.save() // save the user details in the database
-        res.status(201).json({message: "Signup Successfully", success: true})
-    } catch (error) {
-        res.status(500).json({message: "Internal Server Error", success: false})
+  try {
+    const { name, email, password } = req.body;
+    const user = await UserModal.findOne({ email }); // here we check the email user enter is already exist or not if yes then user can directly login
+    // here the logic, if user exist then user can login
+    if (user) {
+      return res.status(409).json({
+        message: "user is already exist, you can login",
+        success: false,
+      });
     }
-}
 
-module.exports = {signup}
+    const userModal = new UserModal({ name, email, password }); // logic => if new user comes then we create a user
+    userModal.password = await bcrypt.hash(password, 10); // use bcrypt to make password in the hash form
+    await userModal.save(); // save the user details in the database
+    res.status(201).json({ message: "Signup Successfully", success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
+};
+
+// login functionality
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModal.findOne({ email });
+    const errMsg = "Auth failed email or password is wrong";
+
+    // if user does not exist then it throws an error message
+    if (!user) {
+      return res.status(403).json({
+        message: errMsg,
+        success: false,
+      });
+    }
+
+    // now we are decrypting the password and check
+
+    const isPasswordEqual = await bcrypt.compare(password, user.password); // here 1st password is coming from user side or server side and second password is coming from database
+
+    // now we will check if both password is equal or not, if both are not equal then
+    if (!isPasswordEqual) {
+      return res.status(403).json({
+        message: errMsg,
+        success: false,
+      });
+    }
+
+    const jwtToken = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(200).json({ message: "Login Successfully", success: true, jwtToken, email, name:user.name });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
+};
+
+module.exports = { signup, login };
--- backend/router/authRouter.js
+++ backend/router/authRouter.js
@@ -1,12 +1,10 @@
-const { signup } = require("../controllers/authController");
-const { signUpValidation } = require("../middlewares/authValidation");
+const { signup, login } = require("../controllers/authController");
+const { signUpValidation, logInValidation } = require("../middlewares/authValidation");
 
 const router = require("express").Router()
 
 
-router.post("/login", (req, res) => {
-    res.send("login success...")
-})
+router.post("/login", logInValidation, login)
 
 router.post("/signup", signUpValidation, signup) // when validation is successful then only user will route to sign up page
 
